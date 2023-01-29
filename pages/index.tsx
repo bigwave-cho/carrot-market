803edd7e6d20c9f899b50f8fb0711200a79206f7
JaehyeonCho93 <hooyaa93@gmail.com>
2023-01-29 16:50:05 +0900

    Responsive 적용해보기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,26 @@
 import { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  // Responsive Modifier
-  // tailwind의 모든 클래스네임은 모바일에 우선 적용
-  // 큰 화면이 후순위가 된다.
+  // Responsive 적용해보자
+  // 중요: 모바일부터 차례대로 늘려가면서 breakpoint를 걸면서 스타일을 수정해주면 되겠다.
   /*
-  Mobile First
-
-기본적으로 Tailwind는 Bootstrap과 같은 다른 프레임워크에서 사용하는 것과 유사한 모바일 우선 breakpoint 시스템을 사용합니다. 이것이 의미하는 바는 접두사가 붙지 않은 유틸리티(예: uppercase)는 모든 화면 크기에 적용되는 반면 접두사가 붙은 유틸리티(예: md:uppercase)는 지정된 breakpoint 이상에서만 적용됩니다.
-
-이 접근 방식이 사람들을 가장 자주 놀라게 하는 부분은 모바일용으로 스타일을 지정하려면 sm: 접두사가 붙은 버전이 아니라 접두사가 없는 버전의 유틸리티를 사용해야 한다는 것입니다. sm을 "작은 화면에서"를 의미하는 것으로 생각하지 마십시오. "작은 breakpoint"로 생각하십시오.
-div class="sm:text-center" => 작은 사이즈 (not 모바일)
-
-이러한 이유로 디자인을 위한 모바일 레이아웃을 먼저 구현한 다음 sm 화면에 적합한 변경 사항을 레이어링한 다음 md 화면 등을 적용하는 것이 좋습니다.
-```
-sm 640px @media (min-width: 640px) { ... }
-md 768px @media (min-width: 768px) { ... }
-lg 1024px @media (min-width: 1024px) { ... }
-xl 1280px @media (min-width: 1280px) { ... }
-2xl 1536px @media (min-width: 1536px) { ... }
-```
+  lg:grid-cols-2 xl:grid-cols-3
+  place-content-center(gird의 속성) 모든 컨텐츠 가운데로.
+  하지만 이 속성이 mobile에 적용되면 이상해지니까 xl로 브레이크 포인트 걸자.
+  
+  mobile에서 portrait / landscape 도 있음.
   */
+
   return (
-    <div className="grid min-h-screen gap-10 bg-slate-400 py-20 px-20">
-      <div className="rounded-3xl bg-white p-6 shadow-xl sm:bg-red-400 sm:hover:bg-pink-900 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-500">
+    <div className="grid min-h-screen gap-10 bg-slate-400 py-20 px-20 lg:grid-cols-2 xl:grid-cols-3 xl:place-content-center">
+      <div className="flex flex-col justify-between rounded-3xl bg-white p-6 shadow-xl">
         <span className="text-2xl font-semibold">Select Item</span>
         <ul>
           <div className="my-2 flex justify-between ">
             <span className="text-gray-500">Grey Chair</span>
             <span className="font-semibold">$19</span>
           </div>
-          <div className="my-2 flex justify-between ">
+          <div className="my-2 flex justify-between">
             <span className="text-gray-500">Grey Chair</span>
             <span className="font-semibold">$19</span>
           </div>
@@ -51,7 +41,7 @@ xl 1280px @media (min-width: 1280px) { ... }
         </button>
       </div>
       <div className="group overflow-hidden rounded-3xl bg-white shadow-xl">
-        <div className="bg-blue-500 p-6 pb-14">
+        <div className="p-6 pb-14 xl:pb-40  portrait:bg-indigo-600 landscape:bg-teal-400">
           <span className="text-2xl text-white">Profile</span>
         </div>
         <div className="relative -top-5 rounded-3xl bg-white p-6">
@@ -72,7 +62,7 @@ xl 1280px @media (min-width: 1280px) { ... }
           </div>
         </div>
       </div>
-      <div className="rounded-3xl bg-white p-6 shadow-xl">
+      <div className="rounded-3xl bg-white p-6 shadow-xl lg:col-span-2 xl:col-span-1">
         <div className="mb-5 flex items-center justify-between">
           <span>⬅️</span>
           <div className="space-x-3">
