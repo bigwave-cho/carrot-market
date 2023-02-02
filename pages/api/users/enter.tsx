222f25a1a0b66195eebd8d5170582fc0103b8865
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-02 23:54:21 +0900

    Twilio SMS 보내기 세팅

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,15 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from '@/libs/server/client';
 import withHandler, { ResponseType } from '@/libs/server/withHandler';
+import twilio from 'twilio';
 
 /*
-Twilio 생성하기!
-접속 -> 아이디만들면 15달러 무료로 줌.
-SID와 토큰을 환경변수에 등록
--> Messaging -> Services에서 서비스 하나 1단계까지만 만들고
--> Try it out의 get setup에서 폰넘버 만들기
--> Try SMS -> 트라이얼 계정이기 때문에 받는 폰 넘버 바꾸기 안됨. 내 폰으로 문자옴.
+Message service sid와 본인 폰넘버도 .env에 추가
+npm install twilio
 */
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -36,6 +35,17 @@ async function handler(
     },
   });
   console.log(token);
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.TWILIO_PHONE_NUMBER!,
+      // 원래는 req.body의 폰 넘버를 받아야 하지만
+      // 계정이 트라이얼이라 내 폰 번호로 보내는 것만 가능하기 때문에
+      // 내 번호 넣음. 가입은 되는데 내 번호로 문자 옴.
+      body: `Your login token is ${payload}`,
+    });
+    console.log(message);
+  }
 
   return res.json({
     ok: true,
