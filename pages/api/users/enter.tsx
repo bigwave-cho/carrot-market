3ae2009ebd2ff8faae87140cc6313d8deadc6365
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-04 00:27:57 +0900

    SendGrid: email setting

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,13 +2,18 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import client from '@/libs/server/client';
 import withHandler, { ResponseType } from '@/libs/server/withHandler';
 import twilio from 'twilio';
+import mail from '@sendgrid/mail';
+import { SyncGrant } from 'twilio/lib/jwt/AccessToken';
 
 /*
-Message service sid와 본인 폰넘버도 .env에 추가
-npm install twilio
+Sendgrid 를 이용한 이메일 인증 기능 구현
+가입 -> Account 생성 -> integration, Web API 클릭
+APIKEY 생성해서 .env에 추가한 후 npm install --save @sendgrid/mail
 */
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+mail.setApiKey(process.env.SEND_GRID_API_KEY!);
 
 async function handler(
   req: NextApiRequest,
@@ -34,17 +39,23 @@ async function handler(
       },
     },
   });
-  console.log(token);
   if (phone) {
     const message = await twilioClient.messages.create({
       messagingServiceSid: process.env.TWILIO_MSID,
       to: process.env.TWILIO_PHONE_NUMBER!,
-      // 원래는 req.body의 폰 넘버를 받아야 하지만
-      // 계정이 트라이얼이라 내 폰 번호로 보내는 것만 가능하기 때문에
-      // 내 번호 넣음. 가입은 되는데 내 번호로 문자 옴.
+
       body: `Your login token is ${payload}`,
     });
     console.log(message);
+  } else if (email) {
+    const email = await mail.send({
+      from: process.env.MY_EMAIL_ADDRESS,
+      to: process.env.MY_EMAIL_ADDRESS,
+      subject: 'Verify mail',
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email);
   }
 
   return res.json({
