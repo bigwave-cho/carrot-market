356f3b8e24471c1e33072995339d387bea798d29
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-04 22:38:27 +0900

    SWR refactoring 완료

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,23 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-
+import { SWRConfig } from 'swr';
 /*
-max-w-xs => max-width: 20rem; 320px 
-max-w-sm => max-width: 24rem;  384px 
-max-w-md => max-width: 28rem;  448px 
-max-w-lg => max-width: 32rem;  512px 
-max-w-full => max-width: 100%;
-max-w-screen-sm => max-width: 640px;
-max-w-screen-md => max-width: 768px;
-max-w-screen-lg => max-width: 1024px;
-max-w-screen-xl => max-width: 1280px;
-
-1. 너비를 최대로 놓고 좌우마진을 오토로 놓으면 div가 중앙으로 옴(당연 block 이어야 함)
-2. 아래처럼 설정하면 모바일로 고정되어 화면 중간에 컴포넌트 위치.
-3. max-w-[] <- 정해놓은 너비까지만 넓어짐.
+SWRConfig
+fetcher 기본값을 정하고 글로벌 옵션 지정도 가능.
 */
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="mx-auto w-full max-w-xl">
-      <Component {...pageProps} />
-    </div>
+    <SWRConfig
+      value={{
+        refreshInterval: 2000,
+        fetcher: (url: string) =>
+          fetch(url).then((response) => response.json()),
+      }}
+    >
+      <div className="mx-auto w-full max-w-xl">
+        <Component {...pageProps} />
+      </div>
+    </SWRConfig>
   );
 }
